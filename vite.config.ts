dd00b5b672fe8994c6171989d41a7007156ee94a
Jimmy García <jimmyegc@gmail.com>
2024-10-13 10:44:48 -0600

    testing gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base:'https://jimmyegc.github.io/react-ngnix-example'
 })
